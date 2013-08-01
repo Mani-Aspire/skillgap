1efd18b2b7bd52b5f829316fd2bd3ec037af1368
Santhosh <santhosh.vengataswam@aspiresys.com>
2013-08-01 17:10:14 +0530

    Have implemented tree structure using jstree plug-in.

---
+++ iConnect.Web/Models/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace iConnect.Presentation.Models
+{
+    public class Node
+    {
+        public string Name { set; get; }
+        public string NodePath { set; get; }
+        public string ParentNodePath { set; get; }
+
+        public IList<Node> Nodes { set; get; }
+
+        public Node()
+        {
+            Nodes = new List<Node>();
+        }
+
+    }
+}
